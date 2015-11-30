89d7367d59b72a9f34017c862863276752046929
Andreas A. Flakstad <andflak@gmail.com>
2015-11-30 11:54:40 +0100

    user page with grid

--- src/cljs/movement/user.cljs
+++ src/cljs/movement/user.cljs
@@ -21,30 +21,33 @@
   (let [pass (atom {})]
     (fn []
       [:div
-       [:h2 "Change password"]
-       [:input {:type "password"
-                :placeholder "old password"
-                :value (:old-pass @pass)
-                :on-change #(swap! pass assoc :old-pass (-> % .-target .-value))}]
-       [:br]
-       [:input {:type "password"
-                :placeholder "new password"
-                :value (:new-pass @pass)
-                :on-change #(swap! pass assoc :new-pass (-> % .-target .-value))}]
-       [:br]
-       [:input {:type "password"
-                :placeholder "confirm new password"
-                :value (:repeat-pass @pass)
-                :on-change #(swap! pass assoc :repeat-pass (-> % .-target .-value))}]
-       [:br]
+       [:div.pure-g
+        [:h4.pure-u "Change password"]]
+       [:div.pure-g
+        [:input.pure-u {:type        "password"
+                 :placeholder "old password"
+                 :value       (:old-pass @pass)
+                 :on-change   #(swap! pass assoc :old-pass (-> % .-target .-value))}]]
+       [:div.pure-g
+        [:input.pure-u {:type        "password"
+                 :placeholder "new password"
+                 :value       (:new-pass @pass)
+                 :on-change   #(swap! pass assoc :new-pass (-> % .-target .-value))}]]
+
+       [:div.pure-g
+        [:input.pure-u {:type        "password"
+                 :placeholder "confirm new password"
+                 :value       (:repeat-pass @pass)
+                 :on-change   #(swap! pass assoc :repeat-pass (-> % .-target .-value))}]]
        (when-let [info (:info @pass)]
-         [:div info])
+         [:div.pure-g [:div.pure-u info]])
        (when-let [error (:error @pass)]
-         [:div.error error])
+         [:div.pure-g [:div.pure-u error]])
        (if (not= (:new-pass @pass) (:repeat-pass @pass))
-         [:div.error "new password mismatch"]
+         [:div.pure-g [:div.pure-u "new password mismatch"]]
          (when (and (not-empty (:old-pass @pass)) (not-empty (:new-pass @pass)))
-           [:a.button.button-primary {:on-click #(update-password! pass)} "Change password"]))])))
+           [:div.pure-g
+            [:a.pure-u.button.button-primary {:on-click #(update-password! pass)} "Change password"]]))])))
 
 (defn user-component []
   (let []
@@ -52,14 +55,16 @@
       [:div#layout {:class (str "" (when (session/get :active?) "active"))}
        [menu-component]
        [:div.content
-        [:h3 (str "Logged in as " (session/get :user))]
-        [:h2 "Logged sessions"]
-        [:ul
+        [:div.pure-g
+         [:h4.pure-u (str "Logged in as " (session/get :user))]]
+        [:div.pure-g
+         [:h4.pure-u "Logged sessions"]]
+        [:ul.pure-g
          (doall
            (for [s (session/get :stored-sessions)]
-             ^{:key s} [:li (str
-                              (:session/timestamp s) " - " (:session/name s) " - " (:session/comment s)
-                              "\t")
-                        [:a {:href (str "/session/" (:session/url s)) :target "_blank"} "View"]]))]
+             ^{:key s}
+             [:li.pure-u
+              (str (:session/timestamp s) " - " (:session/name s) " - " (:session/comment s) "\t")
+              [:a {:href (str "/session/" (:session/url s)) :target "_blank"} "View"]]))]
         [:div
          [change-password]]]])))
